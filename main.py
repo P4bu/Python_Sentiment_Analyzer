29f51398a5d68abe0dd2015be4a79676a5c70ef7
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2024-10-26 12:19:04 -0300

    Modificacion de codigo aplicando principios SOLID

--- main.py
+++ main.py
@@ -2,32 +2,44 @@ from textblob import TextBlob
 from googletrans import Translator
 
 
-def traduccion_texto(texto):
-    traduccion = Translator()
-    texto_traducido = traduccion.translate(texto, src='es', dest='en').text
-    #print(texto_traducido)
-    return texto_traducido
+class TranslatorService:
+    def traduccion_texto(self, texto: str) -> str:
+        traduccion = Translator()
+        #print(texto_traducido)
+        return traduccion.translate(texto, src='es', dest='en').text
 
 
-def analizar_sentimiento(texto_traducido):
+class SentimientoAnalizer:
+    def analizar_sentimiento(texto_traducido):
 
-    blob = TextBlob(texto_traducido)
-    
-    sentimiento = blob.sentiment.polarity
-    #print('Polaridad: ', sentimiento)
+        blob = TextBlob(texto_traducido)
+        sentimiento = blob.sentiment.polarity
+        #print('Polaridad: ', sentimiento)
 
-    if sentimiento > 0:
-        return 'Positivo'
-    elif sentimiento < 0:
-        return 'Negativo'
-    else:
-        return 'Neutral'
+        if sentimiento > 0:
+            return 'Positivo'
+        elif sentimiento < 0:
+            return 'Negativo'
+        else:
+            return 'Neutral'
+
+class Processor:
+    def __init__(self, translator:TranslatorService, analyzer:SentimientoAnalizer):
+        self.translator = translator
+        self.analyzer = analyzer
+
+    def process(self, texto: str) -> str:
+        texto_traducido = self.translator.translate(texto)
+        sentimiento = self.analyzer.analyzer(texto_traducido)
+        return sentimiento    
 
 def run():
     texto = input('Ingrese un texto para revisar: ')
-    texto_traducido = traduccion_texto(texto)
-    sentimiento = analizar_sentimiento(texto_traducido)
+    Translator = TranslatorService()
+    analyzer = SentimientoAnalizer()
+    processor = Processor() 
 
+    sentimiento = processor.process(texto)
     print(f'Sentimiento ->  {sentimiento}' )
 
 if __name__ == '__main__':
